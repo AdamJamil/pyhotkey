91f1a717116fedf89e9d1d16a79356235a891cb7
Adam Jamil <adam.a1d9c@gmail.com>
2021-02-20 20:14:21 -0500

    can view and update alarms

--- main.py
+++ main.py
@@ -29,8 +29,9 @@ class NotepadIO:
         NotepadIO.show(name, content)
         time.sleep(0.1)
         pyautogui.press("end")
-        while GetWindowText(GetForegroundWindow()).split(" ")[-1] == "Notepad":
-            time.sleep(0.5)
+        curr = GetWindowText(GetForegroundWindow())
+        while GetWindowText(GetForegroundWindow()).replace("*", "") == curr:
+            time.sleep(0.1)
         return open(os.getcwd() + "\\" + name + ".txt", "r").read()
 
     @staticmethod
@@ -49,31 +50,82 @@ class AlarmClock:
         self.alarm_thread = AlarmClock.AlarmThread(self)
         self.alarm_thread.start()
 
-    def show_alarms(self):
-        alarms_text = ""
-        label = ""
-        last_day = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=-1)
-        for i, alarm in enumerate(self.alarms):
-            alarm_time = alarm[0]
-            if alarm_time > last_day + timedelta(days=1):
-                last_day = alarm_time.replace(hour=0, minute=0, second=0, microsecond=0)
-                if last_day <= datetime.today():
-                    label = "[today]"
-                elif last_day <= datetime.today() + timedelta(days=1):
-                    label = "[tomorrow]"
-                else:
-                    label = "[" + str(last_day.month) + "/" + str(last_day.day) + "]"
-                alarms_text += label + "\n"
-            alarms_text += "\t[" + str(i+1) + "] " + str(alarm_time.strftime("%I:%M%p"))
-            if alarm[1] != "":
-                alarms_text += ": " + alarm[1]
-            alarms_text += "\n"
-            if alarm[2] != "":
-                alarms_text += "\t\t" + alarm[2].replace("\n", "\n\t\t") + "\n"
-        NotepadIO.show("Alarms", alarms_text)
+    class ShowAlarmThread(threading.Thread):
+        def __init__(self, alarm_clock):
+            super().__init__()
+            self.alarm_clock = alarm_clock
 
-        # you are allowed to edit the alarms
-            # [R]
+        def run(self):
+            alarms_text = ""
+            label = ""
+            last_day = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=-1)
+            for ptr, alarm in enumerate(self.alarm_clock.alarms):
+                alarm_time = alarm[0]
+                if alarm_time > last_day + timedelta(days=1):
+                    last_day = alarm_time.replace(hour=0, minute=0, second=0, microsecond=0)
+                    if last_day <= datetime.today():
+                        label = "[today]"
+                    elif last_day <= datetime.today() + timedelta(days=1):
+                        label = "[tomorrow]"
+                    else:
+                        label = "[" + str(last_day.month) + "/" + str(last_day.day) + "]"
+                    alarms_text += label + "\n"
+                alarms_text += "\t[" + str(ptr+1) + "] " + str(alarm_time.strftime("%I:%M%p"))
+                if alarm[1] != "":
+                    alarms_text += ": " + alarm[1]
+                alarms_text += "\n"
+                if alarm[2] != "":
+                    alarms_text += "\t\t" + alarm[2].replace("\n", "\n\t\t") + "\n"
+            update = NotepadIO.query("Alarms", alarms_text)
+
+            if alarms_text == update:
+                return
+
+            print("Updating alarms")
+            self.alarm_clock.alarms.clear()
+            curr_label = "[today]"
+            lines = update.split("\n")
+            ptr = 0
+            while ptr < len(lines):
+                line = lines[ptr]
+                if len(line) == 0:
+                    ptr += 1
+                    pass
+                elif line[0] == "[":
+                    ptr += 1
+                    curr_label = line.strip()
+                elif line[1] == "[":
+                    if line[2] == "-":
+                        ptr += 1
+                        while ptr < len(lines) and len(lines[ptr]) > 1 and "[" in lines[ptr][:2]:
+                            ptr += 1
+                        continue
+
+                    alarm_time_text = ":".join(line.split(" ")[1].split(":")[:2])
+                    alarm_time = datetime.strptime(alarm_time_text, "%I:%M%p")
+                    ref_time = datetime.now()
+                    day = curr_label[1:-1]
+                    if day == "tomorrow":
+                        ref_time += timedelta(days=1)
+                    elif day != "today":
+                        ref_time = ref_time.replace(month=int(day.split("/")[0]), day=int(day.split("/")[1]))
+
+                    alarm_time = alarm_time.replace(month=ref_time.month, day=ref_time.day, year=ref_time.year)
+
+                    desc = ""
+                    if len(line.split(":")) >= 3:
+                        desc = ":".join(line.split(":")[2:])
+
+                    info = ""
+                    ptr += 1
+                    while ptr < len(lines) and len(lines[ptr]) >= 2 and lines[ptr][:2] == "\t\t":
+                        info += lines[ptr][2:] + "\n"
+                        ptr += 1
+                    if len(info) > 0:
+                        info = info[:-1]
+                    self.alarm_clock.alarms.append([alarm_time, desc, info])
+
+            self.alarm_clock.alarms = sorted(self.alarm_clock.alarms)
 
     class AlarmThread(threading.Thread):
         def __init__(self, alarm_clock):
@@ -148,6 +200,9 @@ class AlarmClock:
     def add_alarm(self):
         AlarmClock.SetAlarmThread(self, "Set_an_alarm!").start()
 
+    def show_alarm(self):
+        AlarmClock.ShowAlarmThread(self).start()
+
 
 class KeyHandler:
     def __init__(self):
@@ -218,7 +273,7 @@ class KeyHandler:
             }),
             frozenset(["F14"]): ddict(lambda: default, {
                 "A": [self.alarm_clock.add_alarm, []],
-                "V": [self.alarm_clock.show_alarms, []],
+                "V": [self.alarm_clock.show_alarm, []],
             }),
         })
 
@@ -252,7 +307,7 @@ class KeyHandler:
         for key in self.binds_up.keys():
             self.binds_up[key]["Capital"] = [self.reset, []]
 
-        # hard reset check (double press esc)
+        # hard reset check (double press esc)es
         for key in self.binds_down.keys():
             self.binds_down[key]["Escape"] = [self.esc_check, []]
 
@@ -267,13 +322,13 @@ class KeyHandler:
 
     def reset(self):
         self.rep = 1
+        self.m_cmps.clear()
         if self.m_thread is not None:
             self.m_thread.join()
         self.curr_mods.remove("Capital")
         return True
 
     def hard_reset(self):
-        print("wtf")
         self.last_press = time.time()
         self.last_esc = time.time()
         self.rep = 1
